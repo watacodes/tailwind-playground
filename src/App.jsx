04a8881a223cc68c57f219d728e468cfcaaf56ff
watacodes <watafromjapan@gmail.com>
2025-01-05 03:19:23 +0900

    initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import "./App.css";
 function App() {
   return (
     <main className="grid place-items-center min-w-full min-h-screen">
-      <h1 className="font-bold text-4xl">TailWind CSS Playground</h1>
+      <h1 className="font-bold text-4xl">TailWind CSS Playground!</h1>
       <div className="bg-red-500 w-40 p-5 border-black-4">box</div>
     </main>
   );
